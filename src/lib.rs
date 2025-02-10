01daa7c2b89acb065975cc64ab57ff87e280a124
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-02-10 13:40:49 -0800

    feat: expose segment type

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ mod core;
 mod reader;
 mod safe;
 
-pub use reader::{RecordIter, RefRecord, Segment, SegmentIter, SraReader};
+pub use reader::{RecordIter, RefRecord, Segment, SegmentIter, SegmentType, SraReader};
 pub use safe::{Error, Result};
